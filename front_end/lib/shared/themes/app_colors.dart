5f90e342661a33dcea35cb1b18487008a96eb7b2
Caroline Souza <caroline.souzab12@gmail.com>
2023-04-24 22:25:28 -0300

    Creating flutter project

---
+++ front_end/lib/shared/themes/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColor
+{
+    static const black = Color(0x00000000);
+    static const white = Color(0x00FFFFFF);
+    static const gray10 = Color(0x001F1B29);
+    static const gray25 = Color(0x002E2A36);
+    static const gray60 = Color(0x00616161);
+    static const gray100 = Color(0x0099969E);
+    static const gray200 = Color(0x00BAB9BD);
+    static const gray300 = Color(0x00616161);
+    static const gray400 = Color(0x00ECECEE);
+    static const gray500 = Color(0x00F3F3F5);
+    
+    static const pinkPrimary = Color(0x00F50057);
+    static const pinkSecond = Color(0x00EC407A);
+
+    static const blueLink = Color(0x001A44AF);
+    static const cyan = Color(0x0080DEEA);
+} 
---
+++ front_end/lib/shared/themes/app_text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles
+{
+    static TextStyle regularHeader1 = TextStyle(
+      fontSize: 25, 
+      fontWeight: FontWeight.normal,
+
+    );
+}
