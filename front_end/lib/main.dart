da21229d420b52ca8652cb65c4524e8d8b23e36d
Honkato <meira.gmrm@hotmail.com>
2023-09-05 19:56:24 -0300

    teste commit

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       color: AppColors.pinkPrimary,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page ihuuu'),
     );
   }
 }
