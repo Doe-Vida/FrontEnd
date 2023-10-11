71d483e61bde9c3e68e9555b2f48aa7be0938fd1
interview-user <caroline.souzab12@gmail.com>
2023-10-10 22:30:21 -0300

    home update

--- front_end/lib/views/pages/home/homePage.dart
+++ front_end/lib/views/pages/home/homePage.dart
@@ -3,6 +3,7 @@ import 'package:front_end/constants/app_colors.dart';
 import 'package:front_end/constants/app_images.dart';
 import 'package:front_end/constants/app_text_styles.dart';
 import 'package:front_end/views/widgets/appbars/custom_appbar.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import '../../../constants/app_icons.dart';
@@ -21,9 +22,9 @@ class _HomePageState extends State<HomePage> {
   String bloodType = "O+";
 
   final List<String> images = [
-    AppImages.FRASES_CONVEY_LOVE,
     AppImages.FRASES_DONATE_BLOOD_LIFE,
     AppImages.FRASES_DONATE_IS_GOOD,
+    AppImages.FRASES_CONVEY_LOVE,
   ];
 
   @override
@@ -62,10 +63,12 @@ class _HomePageState extends State<HomePage> {
                   Container(
                     width: 85,
                     height: 85,
+                    margin: const EdgeInsets.only(right: 10),
                     decoration: const BoxDecoration(
                         shape: BoxShape.circle, color: AppColors.WHITE),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         name,
@@ -82,7 +85,7 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
-            Stack(
+            Column(
               children: [
                 Container(
                   width: double.maxFinite,
@@ -94,126 +97,101 @@ class _HomePageState extends State<HomePage> {
                       color: AppColors.WHITE),
                   transform: Matrix4.rotationZ(-0.04),
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: 15),
-                  width: double.maxFinite,
-                  height: screenSize.height * 0.60,
-                  color: AppColors.WHITE,
-                  child: Column(
-                    children: [
-                      //last donation & blood type
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                lastDonation,
-                                style: AppTextStyles.regularHeader3
-                                    .copyWith(color: AppColors.GRAY25),
-                              ),
-                              Text(
-                                "Última doação",
-                                style: AppTextStyles.regularHeader3
-                                    .copyWith(color: AppColors.GRAY25),
-                              )
-                            ],
-                          ),
-                          Container(
-                            width: 1,
-                            height: 35,
-                            color: AppColors.GRAY200,
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                bloodType,
-                                style: AppTextStyles.regularHeader3
-                                    .copyWith(color: AppColors.GRAY25),
-                              ),
-                              Text(
-                                "Tipo sanguíneo",
-                                style: AppTextStyles.regularHeader3
-                                    .copyWith(color: AppColors.GRAY25),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-
-                      //buttons
-                      Row(
-                        children: [
-                          Container(
-                            width: screenSize.width * 0.3,
-                            height: 150,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  offset: const Offset(0, 3),
-                                  blurRadius: 5,
-                                  spreadRadius: 0,
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 15),
+                    width: double.maxFinite,
+                    color: AppColors.WHITE,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        //last donation & blood type
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  lastDonation,
+                                  style: AppTextStyles.regularHeader3
+                                      .copyWith(color: AppColors.GRAY25),
                                 ),
-                              ],
-                            ),
-                            child: Column(
-                              children: [
-                                AppIcons.fileDocument(AppColors.GRAY25, 70),
                                 Text(
-                                  "Teste aptidão",
-                                  style: AppTextStyles.regularHeader2
+                                  "Última doação",
+                                  style: AppTextStyles.regularHeader3
                                       .copyWith(color: AppColors.GRAY25),
                                 )
                               ],
                             ),
-                          ),
-                          Container(
-                            width: screenSize.width * 0.4,
-                            height: 13,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
+                            Container(
+                              width: 1,
+                              height: 35,
+                              color: AppColors.GRAY200,
                             ),
-                            child: Column(
+                            Column(
                               children: [
-                                AppIcons.fileDocument(AppColors.GRAY25, 70),
                                 Text(
-                                  "Passo a passo",
-                                  style: AppTextStyles.regularHeader2
+                                  bloodType,
+                                  style: AppTextStyles.regularHeader3
+                                      .copyWith(color: AppColors.GRAY25),
+                                ),
+                                Text(
+                                  "Tipo sanguíneo",
+                                  style: AppTextStyles.regularHeader3
                                       .copyWith(color: AppColors.GRAY25),
                                 )
                               ],
                             ),
-                          )
-                        ],
-                      ),
-
-                      Text("Inspire-se", style: AppTextStyles.boldHeader2),
-
-                      //carousel
-                      CarouselSlider(
-                        items: images.map((String image) {
-                          return Container(
-                            margin: EdgeInsets.all(5.0),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              image: DecorationImage(
-                                image: NetworkImage(image),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                        options: CarouselOptions(
-                          autoPlay: true,
-                          aspectRatio: 16 / 9,
-                          enlargeCenterPage: true,
-                          viewportFraction: 0.8,
+                          ],
                         ),
-                      ),
-                    ],
+
+                        //buttons
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ButtonHome(
+                                text: "Teste aptidão",
+                                icon: AppIcons.fileDocument(),
+                                onTap: () {}),
+                            ButtonHome(
+                                text: "Passo a passo",
+                                icon: AppIcons.fileDocument(),
+                                onTap: () {})
+                          ],
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text("Inspire-se",
+                                  style: AppTextStyles.boldHeader2)),
+                        ),
+
+                        //carousel
+                        CarouselSlider(
+                          items: images.map((String image) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(color: AppColors.GRAY400),
+                                image: DecorationImage(
+                                  image: AssetImage(image),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                          options: CarouselOptions(
+                            height: 150,
+                            autoPlay: true,
+                            aspectRatio: 16 / 9,
+                            enlargeCenterPage: true,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 )
               ],
@@ -221,6 +199,34 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ]),
+    );
+  }
+}
+
+class ButtonHome extends StatelessWidget {
+  final String text;
+  final Iconify icon;
+  final VoidCallback onTap;
+  const ButtonHome(
+      {super.key, required this.text, required this.icon, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsetsDirectional.symmetric(vertical: 20, horizontal: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: AppColors.GRAY400)),
+      child: Column(
+        children: [
+          icon,
+          Text(
+            text,
+            style:
+                AppTextStyles.regularHeader3.copyWith(color: AppColors.GRAY25),
+          )
+        ],
+      ),
     );
   }
 }
